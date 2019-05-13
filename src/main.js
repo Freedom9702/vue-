e86c8a4e69e3d35a3c9eb642fd0eb0c2f6380833
Freedom9702 <1872617734@qq.com>
2019-05-13 12:59:52 +0800

    添加vuex

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import MintUI from 'mint-ui'
+import store from './store'
 Vue.use(MintUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router,store,
   components: { App },
   template: '<App/>'
 })
